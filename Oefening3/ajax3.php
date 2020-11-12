cb5e862f6abbb544863320d45e790902ee5dafe1
Sander van Ast <sandervanast1@gmail.com>
2020-11-12 09:45:47 +0100

    :tada: initial boilerplate

---
+++ Oefening3/ajax3.php
@@ -0,0 +1,12 @@
+<?php
+    $myArray = [
+        1 => 'Kees',
+        5 => 'Carla',
+        21 => 'Marco',
+        17 => 'Edwin',
+        30 => 'Nira',
+    ];
+
+    $output = json_encode($myArray);
+    echo $output;
+?>
