05b31a7e0ad0242fd1f9e1bfaa08d632202ca084
Sander van Ast <sandervanast1@gmail.com>
2020-11-25 19:39:36 +0100

    :rocket: exercise 6 done

---
+++ Oefening6/config.inc.php
@@ -0,0 +1,19 @@
+<?php
+$db_hostname = 'localhost';
+$db_username = 'root';
+$db_password = '';
+$db_database = 'spa';
+
+//maak de database-verbinding
+$mysqli = mysqli_connect($db_hostname, $db_username, $db_password, $db_database);
+
+//als de verbinding niet kan gemaakt kan worden: geef een fout melding
+if (!$mysqli) {
+    echo "Error! Er is iets mis met de Databaseverbinding. <br/>"; 
+    exit;
+}
+
+//foutmelding zichtbaar maken
+// error_reporting(E_ALL);
+// ini_set('display_error', '1');
+?>
