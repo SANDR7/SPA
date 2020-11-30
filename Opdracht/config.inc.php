713b5fcda4b6af00a495d091411cea60ad21ed04
Sander van Ast <sandervanast1@gmail.com>
2020-11-30 19:45:00 +0100

    :construction: SPA beginning

--- Opdracht/config.inc.php
+++ Opdracht/config.inc.php
@@ -2,7 +2,7 @@
 $db_hostname = 'localhost';
 $db_username = 'root';
 $db_password = '';
-$db_database = 'test_spa';
+$db_database = 'spa';
 
 //maak de database-verbinding
 $mysqli = mysqli_connect($db_hostname, $db_username, $db_password, $db_database);
