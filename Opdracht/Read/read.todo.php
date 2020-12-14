bff42b335ea3ec2f1ef2ff6b1a66f26b37a36c31
Sander van Ast <sandervanast1@gmail.com>
2020-12-14 16:33:17 +0100

    working read data function

---
+++ Opdracht/Read/read.todo.php
@@ -0,0 +1,15 @@
+<?php
+require '../config.inc.php';
+
+$data = [];
+
+$result = mysqli_query($mysqli, "SELECT * FROM `todo`");
+while ($row = mysqli_fetch_array($result)) {
+    $data[] = $row;
+}
+
+$dist =  json_encode($data);
+
+echo $dist;
+
+
