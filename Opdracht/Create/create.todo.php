fc0ae4285a0e723bcec08d7f7945b6daf8e102a7
Sander <Sandervanast1@gmail.com>
2020-12-28 00:49:50 +0100

    updated file

---
+++ Opdracht/Create/create.todo.php
@@ -0,0 +1,15 @@
+<?php require "../config.inc.php";
+$invoer = htmlentities(
+    $_POST["invoer"],
+    ENT_QUOTES
+);
+
+$result = mysqli_query($mysqli, "INSERT INTO `todo` (`title`) VALUES ('{$invoer}')");
+
+
+if ($result) {
+    echo "OK";
+} else {
+    echo "FOUT";
+}
+
