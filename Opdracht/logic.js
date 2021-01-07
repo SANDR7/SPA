260a28a68ea09b012ba7948a801682482a060622
Sander van Ast <sandervanast1@gmail.com>
2021-01-07 12:39:55 +0100

    :lipstick: bootstrap styling

--- Opdracht/logic.js
+++ Opdracht/logic.js
@@ -1,9 +1,9 @@
 const showTodos = () => {
     $.getJSON("Read/read.todo.php", function (data) {
         //   var output = "<ul>";
-        var Output = `<ul>`;
+        var Output = `<div class='row'>`;
         for (var i in data) {
-            Output += `<li>`;
+            Output += `<div class='col-2 m-2 p-2 d-flex flex-column justify-content-between'>`;
             Output += `<b>${data[i].title}</b>`;
             if (data[i].complete == 1) {
                 Output += `<div>gesloten</div>`;
@@ -11,31 +11,36 @@ const showTodos = () => {
                 Output += `<div>Open</div>`;
             }
             Output += `<div>`;
-            Output += `<button onclick="openEditModel()"       type="button"
-      class="btn btn-primary"
+            Output += `<button onclick="openEditModel()" type="button"
+      class="btn btn-info"
       data-toggle="modal"
-      data-target="#modal1">Edit</button>`;
+      data-target="#modal1">Bewerken</button>`;
             Output += `</div>`;
-            Output += `</li>`;
+            Output += `</div>`;
         }
-        Output += "</ul>";
+        Output += "</div>";
         $("#Dist").html(Output);
     });
 };
 
 const addTodo = () => {
     var invoer = $(`#invoer`).val();
+    if (invoer === "") {
+        $(`#isFinished`).html(`<div class='alert alert-danger my-4'>Het veld is leeg</div>`);
+        return false;
+    }
     $.ajax({
         url: `Create/create.todo.php`,
         method: `POST`,
         data: { invoer: invoer },
     }).done(function (data) {
         if (data == "OK") {
-            $(`#isFinished`).text(`Gelukt!`);
+            $(`#isFinished`).html(`<div class='alert alert-success my-4'>Gelukt!</div>`);
             showTodos();
             $("#invoer").val('');
-        } else {
-            $(`#isFinished`).text(`Er ging iets fout!`);
+        }
+        else {
+            $(`#isFinished`).html(`<div class='alert alert-danger my-4'>Er ging iets fout!</div>`);
         }
     });
 }
