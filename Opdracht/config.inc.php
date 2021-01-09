c4f539151bc9f7de5fd960ae2154ef22ade47f48
Sander van Ast <sandervanast1@gmail.com>
2021-01-09 21:44:37 +0100

    updated file

--- Opdracht/config.inc.php
+++ Opdracht/config.inc.php
@@ -2,7 +2,7 @@
 $db_hostname = 'localhost';
 $db_username = 'root';
 $db_password = '';
-$db_database = 'test_spa';
+$db_database = 'spa';
 
 //maak de database-verbinding
 $mysqli = mysqli_connect($db_hostname, $db_username, $db_password, $db_database);
@@ -16,4 +16,3 @@ if (!$mysqli) {
 //foutmelding zichtbaar maken
 // error_reporting(E_ALL);
 // ini_set('display_error', '1');
-?>
--- Opdracht/logic.js
+++ Opdracht/logic.js
@@ -1,36 +1,36 @@
 const showTodos = () => {
-    $.getJSON("Read/read.todo.php", function (data) {
-        //   var output = "<ul>";
-        var Output = `<div class='row'>`;
-        for (var i in data) {
-            Output += `<div class='col-3 m-4 my-5 p-3 d-flex flex-column justify-content-between'>`;
-            Output += `<b>${data[i].title}</b>`;
-            
-            Output += `<div>`;
-            if (data[i].complete == 1) {
-                Output += `<div class='my-2'>gesloten</div>`;
-            } else {
-                Output += `<div class='my-2'>Open</div>`;
-            }
-            Output += `<div>`;
-            Output += `<button onclick="openEditModel()" type="button"
+  $.getJSON("Read/read.todo.php", function (data) {
+    //   var output = "<ul>";
+    var Output = `<div class='row'>`;
+    for (var i in data) {
+      Output += `<div class='col-3 m-4 my-5 p-3 d-flex flex-column justify-content-between'>`;
+      Output += `<b>${data[i].title}</b>`;
+
+      Output += `<div>`;
+      if (data[i].complete == 1) {
+        Output += `<div class='my-2'>gesloten</div>`;
+      } else {
+        Output += `<div class='my-2'>Open</div>`;
+      }
+      Output += `<div>`;
+      Output += `<button type="button"
             class="btn btn-info"
             data-toggle="modal"
-            data-target="#modal${data[i].id}">Bewerken</button>`;
-            Output += `</div>`;
-            Output += `</div>`;
-            Output += `</div>`;
-            Output += `<div>`;
+            data-target="#modal${data[i].id}">
+               Bewerken
+            </button>`;
+      Output += `</div>`;
+      Output += `</div>`;
+      Output += `</div>`;
+      Output += `<div>`;
 
-            Output += `
-             <div
-      class="modal fade"
+      Output += `
+             <div class="modal fade"
       id="modal${data[i].id}"
       tabindex="-1"
       role="dialog"
       aria-labelledby="modal1-label"
-      aria-hidden="true"
-    >
+      aria-hidden="true">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
@@ -61,38 +61,37 @@ const showTodos = () => {
         </div>
       </div>
     </div>
-
             `;
-            Output += `</div>`;
-        }
-        Output += "</div>";
-        $("#Dist").html(Output);
-    });
+      Output += `</div>`;
+    }
+    Output += "</div>";
+    $("#Dist").html(Output);
+  });
 };
 
 const addTodo = () => {
-    var invoer = $(`#invoer`).val();
-    if (invoer === "") {
-        $(`#isFinished`).html(`<div class='alert alert-danger my-4'>Het veld is leeg</div>`);
-        return false;
+  var invoer = $(`#invoer`).val();
+  if (invoer === "") {
+    $(`#isFinished`).html(`<div class='alert alert-danger my-4'>Het veld is leeg</div>`);
+    return false;
+  }
+  $.ajax({
+    url: `Create/create.todo.php`,
+    method: `POST`,
+    data: { invoer: invoer },
+  }).done(function (data) {
+    if (data == "OK") {
+      $(`#isFinished`).html(`<div class='alert alert-success my-4'>Gelukt!</div>`);
+      showTodos();
+      $("#invoer").val('');
     }
-    $.ajax({
-        url: `Create/create.todo.php`,
-        method: `POST`,
-        data: { invoer: invoer },
-    }).done(function (data) {
-        if (data == "OK") {
-            $(`#isFinished`).html(`<div class='alert alert-success my-4'>Gelukt!</div>`);
-            showTodos();
-            $("#invoer").val('');
-        }
-        else {
-            $(`#isFinished`).html(`<div class='alert alert-danger my-4'>Er ging iets fout!</div>`);
-        }
-    });
+    else {
+      $(`#isFinished`).html(`<div class='alert alert-danger my-4'>Er ging iets fout!</div>`);
+    }
+  });
 }
 const openEditModel = () => {
-    $('#temp').show();
+  $('.model').show();
 }
 
 // const deleteTodo = () => {
